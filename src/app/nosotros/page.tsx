deadede42fed6091be350856738f8635d15744e1
luigiPatitucci <jluigi.patitucci@gmail.com>
2026-08-14 01:58:12 -0300

    add new changes and resolve conflicts

--- src/app/nosotros/page.tsx
+++ src/app/nosotros/page.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { AboutHero } from '@/components/sections/nosotros/AboutHero/AboutHero';
 import { ManifestoBlock } from '@/components/sections/nosotros/ManifestoBlock/ManifestoBlock';
-import { FoundersBlock } from '@/components/sections/nosotros/FoundersBlock/FoundersBlock';
 import { AboutSection } from '@/components/sections/nosotros/AboutSection/AboutSection';
 import { PhilosophySection } from '@/components/sections/nosotros/PhilosophySection/PhilosophySection';
 
@@ -16,9 +15,8 @@ export default function NosotrosPage() {
     <>
       <AboutHero />
       <ManifestoBlock />
-      <FoundersBlock />
-      <PhilosophySection />
       <AboutSection />
+      <PhilosophySection />
     </>
   );
 }
